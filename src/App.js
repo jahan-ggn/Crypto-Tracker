d750911eaac1fb7cdf54a2904631eb090187f719
Jahan Gagan <jahan@mobiux.in>
2022-04-12 15:23:41 +0530

    Removing unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import HomePage from "./Pages/HomePage";
-import CoinPage from "./Pages/CoinPage";
 import { makeStyles } from '@material-ui/core/styles';
 
 function App() {
